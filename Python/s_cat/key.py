df343c543c4848b34f23fb3b529cee562cf154ef
Aaron Watters <awatters@simonsfoundation.org>
2017-06-21 10:04:00 -0400

    add key containers

---
+++ Python/s_cat/key.py
@@ -0,0 +1,99 @@
+
+class Key(object):
+
+    "Abstract superclass: an index key."
+
+    def less_than(self, other):
+        "compare self to other of same kind."
+        raise NotImplementedError("Implement at subclass")
+
+    def to_bytes(self):
+        "compare self to other of same kind."
+        raise NotImplementedError("Implement at subclass")
+
+    def __lt__(self, other):
+        class_test = class_cmp(type(self), type(other))
+        if class_test < 0:
+            return True
+        if class_test > 0:
+            return False
+        return self.less_than(other)
+
+    def value(self):
+        return None
+
+    def __repr__(self):
+        return self.__class__.__name__ + "(" + repr(self.value()) + ")"
+
+
+class NumberKey(Key):
+
+    "An index key containing a number."
+
+    def __init__(self, number):
+        self.number = number
+
+    def less_than(self, other):
+        return self.number < other.number
+
+    def value(self):
+        return self.number
+
+    def to_bytes(self):
+        return b"N " + bytes(self.number)
+
+
+class StringKey(Key):
+
+    "An index key containing a string."
+
+    def __init__(self, string):
+        ts = type(string)
+        if ts is bytes:
+            string = unicode(string, "utf8")
+            ts = type(string)
+        assert ts is unicode
+        self.string = string
+
+    def less_than(self, other):
+        return self.string < other.string
+
+    def value(self):
+        return self.string
+
+    def to_bytes(self):
+        byt = self.string.encode("utf8")
+        length = len(byt)
+        return b"S " + bytes(length) + b"\n" + byt
+
+
+class CompositeKey(Key):
+
+    "An index key containing a two component keys (list cons)."
+    def __init__(self, key1, key2):
+        assert isinstance(key1, Key)
+        assert isinstance(key2, Key)
+        self.key1 = key1
+        self.key2 = key2
+
+    def less_than(self, other):
+        if self.key1 < other.key1:
+            return True
+        if other.key1 < self.key1:
+            return False
+        return (self.key2 < other.key2)
+
+    def value(self):
+        return (self.key1.value(), self.key2.value())
+
+    def to_bytes(self):
+        byt1 = self.key1.to_bytes()
+        byt2 = self.key2.to_bytes()
+        return b"C\n%s\n%s" % (byt1, byt2)
+
+class_order = [NumberKey, StringKey, CompositeKey]
+
+def class_cmp(class1, class2):
+    index1 = class_order.index(class1)
+    index2 = class_order.index(class2)
+    return index1 - index2
---
+++ Python/s_cat/test/test_key.py
@@ -0,0 +1,89 @@
+# This Python file uses the following encoding: utf-8
+
+import unittest
+from .. import key
+
+class TestKeySuperClass(unittest.TestCase):
+
+    def test_not_implemented(self):
+        k1 = key.Key()
+        rpr = repr(k1)
+        self.assertEqual(rpr, "Key(None)")
+        k2 = key.Key()
+        with self.assertRaises(ValueError):
+            lessthan = (k1 < k2)
+        with self.assertRaises(NotImplementedError):
+            lessthan = k1.less_than(k2)
+        with self.assertRaises(NotImplementedError):
+            byt = k1.to_bytes()
+
+class TestNumberKey(unittest.TestCase):
+
+    def test_lt(self):
+        n1 = key.NumberKey(1)
+        self.assertEqual(n1.value(), 1)
+        self.assertEqual(n1.to_bytes(), b"N 1")
+        n2 = key.NumberKey(1 + 1e-10)
+        self.assertTrue(n1 < n2)
+        self.assertFalse(n2 < n1)
+        s1 = key.StringKey("abc")
+        self.assertTrue(n1 < s1)
+        self.assertFalse(s1 < n1)
+        c1 = key.CompositeKey(s1, n2)
+        self.assertTrue(n1 < c1)
+        self.assertFalse(c1 < n1)
+        srt = list(sorted([s1,n1,c1,n2]))
+        self.assertEqual([n1, n2, s1, c1], srt)
+
+class TestStringKey(unittest.TestCase):
+
+    def test_lt(self):
+        s1 = key.StringKey("abc")
+        self.assertEqual(s1.value(), "abc")
+        s2 = key.StringKey("abc0")
+        self.assertTrue(s1 < s2)
+        self.assertFalse(s2 < s1)
+        n1 = key.NumberKey(1)
+        self.assertTrue(n1 < s1)
+        self.assertFalse(s1 < n1)
+        c1 = key.CompositeKey(s2, n1)
+        self.assertTrue(s1 < c1)
+        self.assertFalse(c1 < s1)
+        srt = list(sorted([s1,n1,c1,s2]))
+        self.assertEqual([n1, s1, s2, c1], srt)
+
+    def test_to_bytes(self):
+        s1 = key.StringKey("Äijö")
+        byt = s1.to_bytes()
+        self.assertEqual(b"S 6\n\xc3\x84ij\xc3\xb6", byt)
+
+class TestCompositeKey(unittest.TestCase):
+
+    def test_lt(self):
+        s1 = key.StringKey("abc")
+        n1 = key.NumberKey(1)
+        s2 = key.StringKey("abcd")
+        n2 = key.NumberKey(10)
+        c1 = key.CompositeKey(s1, n1)
+        c3 = key.CompositeKey(s2, n2)
+        c2 = key.CompositeKey(s1, s2)
+        self.assertEqual(c1.value(), ("abc", 1))
+        self.assertTrue(c1 < c2)
+        self.assertFalse(c2 < c1)
+        self.assertTrue(c2 < c3)
+        self.assertFalse(c3 < c2)
+        self.assertTrue(c1 < c3)
+        self.assertFalse(c3 < c1)
+        self.assertTrue(n1 < c3)
+        self.assertFalse(c3 < n1)
+        self.assertTrue(s1 < c3)
+        self.assertFalse(c3 < s1)
+        srt = list(sorted([c3,c2,n2,s1,n1,c1,s2]))
+        self.assertEqual([n1,n2,s1,s2,c1,c2,c3], srt)
+
+    def test_to_bytes(self):
+        s1 = key.StringKey("Äijö")
+        n2 = key.NumberKey(10)
+        c1 = key.CompositeKey(n2, s1)
+        byt = c1.to_bytes()
+        self.assertEqual(b'C\nN 10\nS 6\n\xc3\x84ij\xc3\xb6', byt)
